6fae883d72ee81a9eaf18d6e4d59bce8fd0ebbf0
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2018-08-12 23:01:55 +0200

    Add more meta data to description

--- src/Description.js
+++ src/Description.js
@@ -7,16 +7,21 @@ class Description extends Component {
     return <div >Click on a node to see something here.</div >;
   }
 
-  descriptionPanel() {
+  descriptionPanel({ meta }) {
     return (
       <div className='description-panel' >
-        <h2 >{this.props.service.meta.name}</h2 >
+        <h2 >{meta.name}</h2 >
+        <div ><b > Team: </b > {meta.team}</div >
+        <div ><b > TechStack: </b > {meta.techStack}</div >
+        <div ><b > Git Repo: </b > {meta.gitRepo}</div >
+        <div ><b > External Systems: </b > {meta.externalSystems}</div >
+
       </div >
     )
   }
 
   render() {
-    return this.props.service ? this.descriptionPanel() : this.noNodeSelected();
+    return this.props.service ? this.descriptionPanel(this.props.service) : this.noNodeSelected();
   }
 }
 
